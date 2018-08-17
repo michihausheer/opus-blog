6f7f56c2e05e2ff0ef256b7a366d4fee03f59779
Michi Hausheer <Michi Hausheer@LAPTOP-2CPT5V1O.mshome.net>
2018-08-17 09:25:44 +0200

    2-AcyclicDeps
    - Added lookup of OngoingSalesCalculator service to inventory

--- 2-AcyclicDeps/Callback/opus.inventory/src/com/opus/webshop/inventory/Inventory.java
+++ 2-AcyclicDeps/Callback/opus.inventory/src/com/opus/webshop/inventory/Inventory.java
@@ -1,5 +1,7 @@
 package com.opus.webshop.inventory;
 
+import java.util.ServiceLoader;
+
 import com.opus.webshop.product.Product;
 
 /**
@@ -8,6 +10,12 @@ import com.opus.webshop.product.Product;
 public class Inventory {
 
 	private OngoingSalesCalculator ongoingSalesCalculator;
+	
+	public Inventory()
+	{
+		ServiceLoader<OngoingSalesCalculator> locator = ServiceLoader.load(OngoingSalesCalculator.class);
+		this.ongoingSalesCalculator =  locator.iterator().next();
+	}
 	
 	/**
 	 * Returns how many items of the given product are available.
